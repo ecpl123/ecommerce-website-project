80b3504defdcf777bb43e2556333097a994d4e41
ecpl123 <ecygielski@gmail.com>
2023-06-29 15:38:43 -0500

    added more component examples and commented out old code for Greet, Person, and PersonList

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,43 @@
 import './App.css';
-import { Person } from './components/Person';
-import { Greet } from './components/Greet';
-import { PersonList } from './components/PersonList';
+// import { Person } from './components/Person';
+// import { Greet } from './components/Greet';
+// import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
 
 
 function App() {
-  const personName = {
-    first: 'Eryk',
-    last: 'Cygielski'
-  }
+  // const personName = {
+  //   first: 'Eryk',
+  //   last: 'Cygielski'
+  // }
   
-  const nameList = [
-    {
-      first: 'Peter',
-      last: 'Parker'
-    },
-    {
-      first: 'Bruce',
-      last: 'Wayne'
-    },
-    {
-      first: 'Tony',
-      last: 'Stark'
-    },
-    {
-      first: 'Eryk',
-      last: 'Cygielski'
-    }
-  ]
+  // const nameList = [
+  //   {
+  //     first: 'Peter',
+  //     last: 'Parker'
+  //   },
+  //   {
+  //     first: 'Bruce',
+  //     last: 'Wayne'
+  //   },
+  //   {
+  //     first: 'Tony',
+  //     last: 'Stark'
+  //   },
+  //   {
+  //     first: 'Eryk',
+  //     last: 'Cygielski'
+  //   }
+  // ]
+
 
 
   return (
     <div className="App">
-      <Greet name='Timmy' messageCount={20} isLoggedIn={false}/>
+      <Status status = 'success' />
+      {/* <Greet name='Timmy' messageCount={20} isLoggedIn={false}/>
       <Person name={personName}/>
-      <PersonList names={nameList}/>
+      <PersonList names={nameList}/> */}
     </div>
   );
 }
