56c7dd1522650fc8d7f920333530c130937064fe
ecpl123 <ecygielski@gmail.com>
2023-06-28 22:48:45 -0500

    Added Greet Component
    learned about typescript typing, object props with string, number, and boolean,
    and made a ternery operation which checks if the user is logged in to display appropriate info.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Greet } from './components/Greet';
 
 function App() {
   return (
     <div className="App">
+      <Greet name='Timmy' messageCount={20} isLoggedIn={false}/>
     </div>
   );
 }
